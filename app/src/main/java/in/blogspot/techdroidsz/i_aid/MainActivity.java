492cf23396045351e520631315e622b775bc5efb
Geeky4 <abhishek46810@gmail.com>
2019-03-28 00:59:53 +0530

    Implemented the WEBVIEW.

--- app/src/main/java/in/blogspot/techdroidsz/i_aid/MainActivity.java
+++ app/src/main/java/in/blogspot/techdroidsz/i_aid/MainActivity.java
@@ -1,13 +1,43 @@
 package in.blogspot.techdroidsz.i_aid;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myWebView=(WebView)findViewById(R.id.webView);
+        WebSettings webSettings= myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://35.204.90.246/Homepage.php");
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override public void onReceivedSslError(WebView v, SslErrorHandler handler, SslError er)
+            {
+                handler.proceed();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
